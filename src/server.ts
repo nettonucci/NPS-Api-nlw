c5b680444331c25e136bf2c8b1f13ebb914b8477
nettonucci <nettonucci@gmail.com>
2021-02-22 15:41:21 -0300

    Iniciando servidor

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express()
 
+app.get('/', (req, res) => {
+    return res.json({ message: "Ola Mundo"})
+})
+
+app.post('/', (req, res) => {
+    return res.json({menssage: 'Os dados foram salvos com sucesso!'})
+})
+
 app.listen(3333, () => console.log('Server id running!'))
