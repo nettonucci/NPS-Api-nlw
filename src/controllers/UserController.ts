d9abff035575ddc1e4e25c65b15f6d348ff78046
nettonucci <nettonucci@gmail.com>
2021-02-23 10:07:45 -0300

    Criado entidades e regras

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,28 @@
 import {Request, Response} from 'express'
+import { getRepository } from 'typeorm'
+import { User } from '../models/user'
 
 class UserController{
     async create(request: Request, response: Response) {
-        
+        const {name, email} = request.body
+
+        const usersRepository = getRepository(User);
+
+        const userAlreadyExist = await usersRepository.findOne({
+            email
+        })
+
+        if(userAlreadyExist) {
+            return response.status(400).json({
+                error: "User already exists!"
+            })
+        }
+
+        const user = usersRepository.create({name, email})
+
+        await usersRepository.save(user);
+
+        return response.json(user)
     }
 
 }
